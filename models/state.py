c874d45e38c0ef95619db209540e265753389758
Jinji Zhang <362@holbertonschool.com>
2019-01-07 23:22:42 +0000

    Removed extra import statement

--- models/state.py
+++ models/state.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    __tablename__ =  'states'
+    __tablename__ = 'states'
     name = Column('name', String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref='states')
