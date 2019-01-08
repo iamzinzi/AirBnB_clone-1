7a463e0c2e899e8166b46b5ef98475f5b9efe845
Jian <40183608+TrieToSucceed@users.noreply.github.com>
2019-01-08 13:42:17 -0800

    Revert "Update models"

--- models/review.py
+++ models/review.py
@@ -4,7 +4,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-
 class Review(BaseModel, Base):
     """This is the class for Review
     Attributes:
@@ -13,16 +12,8 @@ class Review(BaseModel, Base):
         text: review description
     """
     __tablename__ = 'reviews'
-    place_id = Column(
-            'place_id',
-            String(60),
-            ForeignKey('places.id'),
-            nullable=False
-        )
-    user_id = Column(
-            'user_id',
-            String(60),
-            ForeignKey('users.id'),
-            nullable=False
-        )
+    place_id = Column('place_id', String(60), ForeignKey('places.id'),
+            nullable=False)
+    user_id = Column('user_id', String(60), ForeignKey('users.id'),
+            nullable=False)
     text = Column('text', String(1024), nullable=False)
