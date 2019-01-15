0ba8c6c7a049923f020f412b2c516bd3dc4a54d8
Jinji Zhang <362@holbertonschool.com>
2019-01-15 17:34:41 +0000

    Refactor code: now flexible with any path arguments passed to do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from fabric.api import local, put, run, env
+from fabric.api import *
 from datetime import datetime
 import os
 
-env.user = 'ubuntu'
 env.hosts = ['34.73.14.116', '34.73.100.50']
+env.user = 'ubuntu'
 
 
 def do_pack():
@@ -31,25 +31,38 @@ def do_deploy(archive_path):
     """Distributes an archive to your web servers.
     Returns True if all operations have been done correctly; else, False.
     """
-    try:
-        if archive_path is None:
-            raise Exception
-        upload = put(archive_path, '/tmp/web_static_20190115005446.tgz')
-        if upload.failed:
-            raise Exception
-        x = run('mkdir -p /data/web_static/releases/web_static_20190115005446')
-        if x.failed:
-            return False
-        run('tar -xzf /tmp/web_static_20190115005446.tgz -C '
-            '/data/web_static/releases/web_static_20190115005446')
-        run('rm -rf /tmp/web_static_20190115005446.tgz')
-        run('mv /data/web_static/releases/web_static_20190115005446/web_static'
-            '/* /data/web_static/releases/web_static_20190115005446/')
-        run('rm -rf /data/web_static/releases/web_static_20190115005446/'
-            'web_static')
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/web_static_20190115005446/ '
-            '/data/web_static/current')
-        return True
-    except:
+    if archive_path is None:
         return False
+
+    # Upload archive to servers /tmp/ dir
+    upload = put(archive_path, '/tmp/')
+    if upload.failed:
+        return False
+
+    # Building path based on given archive path (contains .tgz extension)
+    new_path = archive_path.split('/')[-1]
+    # Remove .tgz extension (i.e. Something like web_static_20190115005446)
+    folder = new_path.split('.')[0]
+
+    # Create folder
+    command = 'mkdir -p /data/web_static/releases/{}'.format(folder)
+    stat = run(command)
+    if stat.failed:
+        return False
+
+    # Uncompress archive and clean up
+    command = f'tar -xzf /tmp/{new_path} -C /data/web_static/releases/{folder}'
+    run(command)
+    run(f'rm -rf /tmp/{new_path}')
+    command = 'mv /data/web_static/releases/{}/web_static/* '.format(folder)
+    command += '/data/web_static/releases/{}/'.format(folder)
+    run(command)
+    cleanup = 'rm -rf /data/web_static/releases/{}/web_static'.format(folder)
+    run(cleanup)
+
+    # Delete old sym link and create a new one
+    run('rm -rf /data/web_static/current')
+    command = 'ln -s /data/web_static/releases/{}/ '.format(folder)
+    command += '/data/web_static/current'
+    run(command)
+    return True
