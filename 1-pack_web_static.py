618efd8a01c7f8eeeafeda9b53a69bd11b52114b
Jinji Zhang <362@holbertonschool.com>
2019-01-15 00:49:34 +0000

    Add conditional statement to check if dir exists before creating

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from fabric.api import local
 from datetime import datetime
-
+import os
 
 def do_pack():
     """enerates a .tgz archive from the contents of the web_static folder
@@ -12,11 +12,11 @@ def do_pack():
     Otherwise, it should return None
     """
     try:
-        local("mkdir versions")
+        if not os.path.exists("./versions"):
+            local("mkdir versions")
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         file_name = "web_static_" + date + ".tgz"
         command = "tar -cvzf " + "./versions/" + file_name + " ./web_static"
-        local(command)
         return "versions/" + file_name
     except:
         return None
