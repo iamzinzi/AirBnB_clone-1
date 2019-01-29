f1a31c364148a5d5f6dea439ad884274264cebeb
Jinji Zhang <362@holbertonschool.com>
2019-01-29 23:11:33 +0000

    1st attempt at flask web application connected to mysql db

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+data = storage.all('State')
+
+@app.teardown_appcontext
+def remove_session(response_or_exc):
+    """Removes the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def show_states():
+    """Shows all State instances
+    """
+    states = []
+    for k, v in data.items():
+        states.append(v)
+    return render_template('7-states_list.html', states=states)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
