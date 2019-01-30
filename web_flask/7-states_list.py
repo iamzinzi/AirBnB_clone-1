1585fb475000a55ee3c4956001d48b08de65f270
Jinji Zhang <362@holbertonschool.com>
2019-01-30 01:58:19 +0000

    Add more documentation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""Starts a Flask web application that renders HTML containing all States"""
 from flask import Flask, render_template
 from models import storage
 
